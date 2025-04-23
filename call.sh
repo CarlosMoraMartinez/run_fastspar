f2859b58384b42d011faa3861033905d09802b18
Carlos Mora Martínez <carmoma9@gmail.com>
2025-04-23 13:09:18 +0200

    added parallel temporal dir

--- call.sh
+++ call.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INDIR=/media/DATOS22T/cmora/CORALS/inputdata_fastspar
-OUTDIR=FastsparFull1
+OUTDIR=FastsparFull2
 if [ -d "$OUTDIR" ]; then
     rm -rf "$OUTDIR"
 fi
@@ -11,29 +11,29 @@ python call_fastspar.py -o $OUTDIR \
     -a $INDIR/remove_tanda2_otus.tsv \
     -s age_class2 \
     --cleanup F \
-    --nrand 1000 \
+    --nrand 10000 \
     --iterations 50 \
     --exclusion_iterations 10 \
     --exclusion_threshold 0.1 \
     --seed 123 \
-    --iterations_parallel 5 \
+    --iterations_parallel 10 \
     --threads 64
 
 
-# OUTDIR=FastsparAgeGroup1
-# if [ -d "$OUTDIR" ]; then
-#     rm -rf "$OUTDIR"
-# fi
-# 
-# python call_fastspar.py -o $OUTDIR \
-#     -m $INDIR/remove_tanda2_metad.tsv \
-#     -a $INDIR/remove_tanda2_otus.tsv \
-#     -s age_class2 \
-#     --cleanup F \
-#     --nrand 1000 \
-#     --iterations 50 \
-#     --exclusion_iterations 10 \
-#     --exclusion_threshold 0.1 \
-#     --seed 123 \
-#     --iterations_parallel 5 \
-#     --threads 64
+OUTDIR=FastsparAgeGroup2
+if [ -d "$OUTDIR" ]; then
+    rm -rf "$OUTDIR"
+fi
+
+python call_fastspar.py -o $OUTDIR \
+    -m $INDIR/remove_tanda2_metad.tsv \
+    -a $INDIR/remove_tanda2_otus.tsv \
+    -s age_class2 \
+    --cleanup F \
+    --nrand 10000 \
+    --iterations 50 \
+    --exclusion_iterations 10 \
+    --exclusion_threshold 0.1 \
+    --seed 123 \
+    --iterations_parallel 10 \
+    --threads 64
--- call_fastspar.py
+++ call_fastspar.py
@@ -77,7 +77,8 @@ def get_fastspar_commands(fname, outdir, fastspar_args, cleanup = True):
             f"--prefix {btdir}/{sname} "
             f"-s {fastspar_args['seed']} ")
     
-    cmd3 = ("parallel fastspar --yes --otu_table {} "
+    cmd3 = (f"parallel --tmpdir {outdir}/tmp "
+            "fastspar --yes --otu_table {} "
             f"--correlation {btcordir}"
             "/cor_{/} "
             f"--covariance {btcordir}"
@@ -157,10 +158,15 @@ def main():
         logger.log(f"Directory created: {outdir}", bcolors.OKBLUE)
     except:
         logger.log(f"Directory creation error: {outdir}", bcolors.FAIL)
+    try:
+        os.mkdir(outdir + "/tmp")
+        logger.log(f"Directory created: {outdir}/tmp", bcolors.OKBLUE)
+    except:
+        logger.log(f"Directory creation error: {outdir}/tmp", bcolors.FAIL)
     
     if cleanup:
         logger.log("WARNING: Cleaning intermediate files!", bcolors.WARNING)
-        
+
     fastspar_args = {}
     fastspar_args["seed"] = args.seed
     fastspar_args["num_random"] = args.nrand
